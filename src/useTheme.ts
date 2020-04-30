7e69ad41d7d72a3c5441319e8152c154f8952a2b
Brendan McNamara <me@brendan9.com>
2020-04-30 12:42:08 -0700

    Saves theme in local storage

--- src/useTheme.ts
+++ src/useTheme.ts
@@ -2,9 +2,12 @@ import { useEffect, useState } from 'react';
 
 export type Theme = 'Light' | 'Dark';
 
+const THEME_LOCAL_STORAGE_KEY = 'v1.theme';
+
 const callbacks: Array<(theme: Theme) => void> = [];
 
-let currentTheme: Theme = 'Light';
+let currentTheme: Theme =
+  localStorage.getItem(THEME_LOCAL_STORAGE_KEY) === 'Dark' ? 'Dark' : 'Light';
 
 export default function useTheme(): [Theme, (theme: Theme) => void] {
   const [theme, setThemeLocal] = useState(currentTheme);
@@ -44,6 +47,8 @@ export function toggleTheme(theme: Theme): Theme {
 
 function setThemeGlobal(theme: Theme) {
   currentTheme = theme;
+  localStorage.setItem(THEME_LOCAL_STORAGE_KEY, theme);
+
   for (const cb of callbacks) {
     cb(theme);
   }
